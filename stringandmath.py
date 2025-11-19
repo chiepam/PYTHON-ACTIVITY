1d6dea42b2337b81239740c9839cf59a86cd0106
chie <francisannemanigbas@gmail.com>
2025-11-20 07:48:08 +0800

    String and Math Function

---
+++ stringandmath.py
@@ -0,0 +1,11 @@
+import math
+
+num_str = input("Enter a positive number: ")
+
+num = float(num_str)
+
+sqrt_result = math.sqrt(num)
+
+result_str = f"The square root of {num} is {sqrt_result:.2f}"
+
+print(result_str)
